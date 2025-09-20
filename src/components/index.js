168cfce50e1ec29b581ede196df25a393780e8ab
P4ndro <sandro02050502@gmail.com>
2025-09-21 03:27:12 +0400

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Billing from "./Billing";
+
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import CTE from "./CTE";
+import Footer from "./Footer";
+import Feedback from "./Feedback";
+import GetStarted from "./GetStarted";
+
+export {
+  Navbar,
+  Hero, 
+    Stats,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTE,
+    Footer,
+    Feedback,
+    GetStarted
+};
